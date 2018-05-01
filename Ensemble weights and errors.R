cb8ae0d4a05ecd3260a71f46060e1e57d3aab9ff
Emily Garner <emilyg1995@yahoo.com>
2018-05-01 17:38:40 -0500

    EBMA Outputs

--- Ensemble weights and errors.R
+++ Ensemble weights and errors.R
@@ -21,11 +21,11 @@ regress.func <- function(Y, preds.var){
 }
 
 
-###excluding KRLS
+###### excluding KRLS regress.func
 
 Y.final<- approve_bi<- ifelse(svdat$approval<3, 1, 0)#line 292 of rep code 
 
-excluding_8<- results[,c(1:6,8:9),]
+excluding_7<- results[,c(1:6,8:9),]
 
 set.seed(10)
 seednum = sample(10000,num.boostraps)
@@ -42,7 +42,7 @@ for (i in 1:num.boostraps){
 
 regress.func.results<- matrix(nrow = 500, ncol = 8)
 for(i in 1:500){
-  regress.func.results[i,] <- regress.func(Y.boostrap[,i], excluding_8[,,i])
+  regress.func.results[i,] <- regress.func(Y.boostrap[,i], excluding_7[,,i])
 }
 
 
@@ -56,7 +56,54 @@ for (i in 1:8){
 mean.coefs_no7
 error_no7
 
-###### With KRLS
+
+
+###### excluding KRLS EBMA
+
+#install.packages("EBMAforecast")
+library(EBMAforecast)
+
+
+round_estimates <- function(x){
+  if (x >= 1 | x <= 0){
+    return(round(x)+0.001)}
+  else {return(x)}
+}
+
+
+rounded_output <- array(dim = c(1074,8,500))
+for (i in 1:length(excluding_7)){
+  rounded_output[i] <- round_estimates(excluding_7[i])
+}
+rounded_output
+
+
+EBMA_results_no7 <- data.frame()
+Names <- c("Lasso", "Elastic Net a = .5", "Elastic Net a = .25", "Bayesian GLM", "BART", "Random Forest", "SVM_SMO", "Simple Average")
+for(i in 1:500){
+  results_slice <- rounded_output[,,i]
+  ForecastData <- makeForecastData(.predCalibration = results_slice, .outcomeCalibration = Y.boostrap[,i], .modelNames = Names)
+  myCal <- calibrateEnsemble(ForecastData)
+  weights <- myCal@modelWeights
+  EBMA_results_no7 <- rbind(EBMA_results_no7, weights)
+}
+colnames(EBMA_results_no7) <- Names
+head(EBMA_results_no7)
+
+
+mean_coefs_EBMA_no7 = numeric(8)
+error_EBMA_no7 = numeric(8)
+for (i in 1:8){
+  error_EBMA_no7[i] =sd(EBMA_results_no7[,i])
+  mean_coefs_EBMA_no7[i] = mean(EBMA_results_no7[,i])
+}
+
+mean_coefs_EBMA_no7
+error_EBMA_no7
+
+
+###### including KRLS regress.func
+
 Y.final<- approve_bi<- ifelse(svdat$approval<3, 1, 0)#line 292 of rep code 
 
 
@@ -89,7 +136,44 @@ for (i in 1:9){
 
 plotting_data<- as.data.frame(mean.coefs)
 plotting_data
-
 error
 
-############ Determining weights and errors for EBMA
+
+###### including KRLS EBMA
+
+round_estimates <- function(x){
+  if (x <= 0){
+    return(0.001)}
+  if (x >= 1){
+    return(0.999)}
+  else {return(x)}
+}
+
+rounded_output <- array(dim = c(1074,9,500))
+for (i in 1:length(results)){
+  rounded_output[i] <- round_estimates(results[i])
+}
+rounded_output
+
+
+EBMA_results <- data.frame()
+Names = c("Lasso", "Elastic Net a = .5", "Elastic Net a = .25", "Bayesian GLM", "BART", "Random Forest",  "KRLS", "SVM_SMO", "Simple Average")
+for(i in 1:500){
+  results_slice <- rounded_output[,,i]
+  ForecastData <- makeForecastData(.predCalibration = results_slice, .outcomeCalibration = Y.boostrap[,i], .modelNames = Names)
+  myCal <- calibrateEnsemble(ForecastData)
+  weights <- myCal@modelWeights
+  EBMA_results <- rbind(EBMA_results, weights)
+}
+colnames(EBMA_results) <- Names
+EBMA_results
+
+mean_coefs_EBMA = numeric(9)
+error_EBMA = numeric(9)
+for (i in 1:9){
+  error_EBMA[i] =sd(EBMA_results[,i])
+  mean_coefs_EBMA[i] = mean(EBMA_results[,i])
+}
+
+mean_coefs_EBMA
+error_EBMA
